8c142e0a4d86570bc2372f07553bfd34603e8284
Jort Willemsen <jort@maurice-nienke.nl>
2022-10-06 14:43:45 +0200

    Add Api project

---
+++ Template.Api/Template.Api.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Template.Api.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
